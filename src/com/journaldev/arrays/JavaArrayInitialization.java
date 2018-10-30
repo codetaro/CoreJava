fbf1b5d208bc93a3f747d090c24ca5317d14a92f
dyuan <dennis.yuan@terrapages.com>
2018-10-30 18:07:23 +1100

    https://www.journaldev.com/744/java-array-of-arraylist-of-array

---
+++ src/com/journaldev/arrays/JavaArrayInitialization.java
@@ -0,0 +1,24 @@
+package com.journaldev.arrays;
+
+public class JavaArrayInitialization {
+
+    public static void main(String[] args) {
+
+        int[] arrInt = new int[5];
+
+        String[] strArr;
+        strArr = new String[4];
+
+        int[][] twoArrInt = new int[4][5];
+
+        int[][] twoIntArr = new int[2][];
+        twoIntArr[0] = new int[2];
+        twoIntArr[1] = new int[3];
+
+        int[] arrI = {1, 2, 3};
+        int[][] arrI2 = {{1, 2}, {1, 2, 3}};
+
+//        int[] twoArrInt1[] = new int[4][5];
+//        int twoIntArr2[][] = new int[5][];
+    }
+}
---
+++ src/com/journaldev/arrays/MultidimensionalArrayExample.java
@@ -0,0 +1,52 @@
+package com.journaldev.arrays;
+
+public class MultidimensionalArrayExample {
+
+    public static void main(String[] args) {
+
+        /* two dimensional array of primitive type
+        int[][] arr = new int[2][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = j;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println("");
+        }*/
+
+        /* two dimensional array of objects
+        String[][] arrStr = new String[3][4];
+        for (int i = 0; i < arrStr.length; i++) {
+            for (int j = 0; j < arrStr[i].length; j++) {
+                arrStr[i][j] = "Str" + j;
+                System.out.print(arrStr[i][j] + " ");
+            }
+            System.out.println("");
+        }*/
+
+        int[][] arrInt = {{1, 2}, {3, 4, 5}};
+        for (int i = 0; i < arrInt.length; i++) {
+            for (int j = 0; j < arrInt[i].length; j++) {
+                System.out.print(arrInt[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+        int[][] arrMulti = new int[2][];
+
+        arrMulti[0] = new int[2];
+        arrMulti[1] = new int[3];
+
+        arrMulti[0][0] = 1;
+        arrMulti[0][1] = 2;
+        arrMulti[1][0] = 3;
+        arrMulti[1][1] = 4;
+        arrMulti[1][2] = 5;
+        for (int i = 0; i < arrMulti.length; i++) {
+            for (int j = 0; j < arrMulti[i].length; j++) {
+                System.out.print(arrMulti[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
