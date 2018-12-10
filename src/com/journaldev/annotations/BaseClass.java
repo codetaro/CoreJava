3be2e510f5cd3fe1dd8336117670ffb4b79f3f97
iquesp <codetaro@gmail.com>
2018-12-11 00:29:55 +1100

    https://www.journaldev.com/842/how-to-get-file-extension-in-java

--- src/com/journaldev/annotations/BaseClass.java
+++
@@ -1,8 +0,0 @@
-package com.journaldev.annotations;
-
-public class BaseClass {
-
-    public void doSomething(Object str) {
-        System.out.println("Base impl:" + str);
-    }
-}
--- src/com/journaldev/composition/TestPerson.java
+++
@@ -1,9 +0,0 @@
-package com.journaldev.composition;
-
-public class TestPerson {
-
-    public static void main(String[] args) {
-        Person person = new Person();
-        long salary = person.getSalary();
-    }
-}
