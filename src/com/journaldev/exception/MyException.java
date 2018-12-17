fdbedbd60bdd1865415cbe4a659388f0f5e3b348
Dennis Yuan <codetaro@gmail.com>
2018-12-18 00:36:18 +1100

    https://www.journaldev.com/14538/java-lang-nosuchmethoderror

--- src/com/journaldev/exception/MyException.java
+++ src/com/journaldev/exception/MyException.java
@@ -1,4 +1,17 @@
 package com.journaldev.exception;
 
-public class MyException {
+public class MyException extends Exception {
+
+    private static final long serialVersionUID = 4664456874499611218L;
+
+    private String errorCode = "Unknown_Exception";
+
+    public MyException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
 }
