8823b4ae59f88f943afeeb9ff3d76aa9879570c6
Dennis Yuan <codetaro@gmail.com>
2018-12-18 23:35:42 +1100

    https://www.journaldev.com/1076/java-threadlocal-example

--- src/com/journaldev/concurrency/SyncronizedMethod.java
+++ src/com/journaldev/concurrency/SyncronizedMethod.java
@@ -1,15 +1,35 @@
 package com.journaldev.concurrency;
 
+import java.util.Arrays;
+
 public class SyncronizedMethod {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String[] arr = {"1", "2", "3", "4", "5", "6"};
         HashMapProcessor hmp = new HashMapProcessor(arr);
+        Thread t1 = new Thread(hmp, "t1");
+        Thread t2 = new Thread(hmp, "t2");
+        Thread t3 = new Thread(hmp, "t3");
+        long start = System.currentTimeMillis();
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        // wait for threads to finish
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println("Time taken= " + (System.currentTimeMillis() - start));
+
+        // check the shared variable value now
+        System.out.println(Arrays.asList(hmp.getMap()));
     }
 }
 
 class HashMapProcessor implements Runnable {
 
+    private Object lock = new Object();
     private String[] strArr = null;
 
     public HashMapProcessor(String[] strArr) {
@@ -30,6 +50,20 @@ class HashMapProcessor implements Runnable {
             // process data and append thread name
             processSomething(i);
             addThreadName(i, name);
+        }
+    }
+
+    private void processSomething(int index) {
+        try {
+            Thread.sleep(index * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addThreadName(int i, String name) {
+        synchronized (lock) {
+            strArr[i] = strArr[i] + ":" + name;
         }
     }
 }
