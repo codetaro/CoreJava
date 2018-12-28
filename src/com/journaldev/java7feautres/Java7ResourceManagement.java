703e97ebcce8b58f1f1eefd8b696e3d337049a8e
iquesp <dennis.yuan@terrapages.com>
2018-12-28 13:16:03 +1100

    Java 7 has introduced a new interface java.lang.AutoCloseable

---
+++ src/com/journaldev/java7feautres/Java7ResourceManagement.java
@@ -0,0 +1,24 @@
+package com.journaldev.java7feautres;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
+public class Java7ResourceManagement {
+
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new FileReader("tmp/journaldev.txt"));
+                BufferedWriter bw = Files.newBufferedWriter(
+                        FileSystems.getDefault().getPath("tmp/journaldev.txt"), Charset.defaultCharset())
+        ) {
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
