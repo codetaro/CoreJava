44eb1f3c9a479256fb06faac05a41e16394a2474
iquesp <codetaro@gmail.com>
2019-01-06 00:47:42 +1100

    https://www.journaldev.com/2800/java-8-date-localdate-localdatetime-instant

---
+++ src/com/journaldev/java8features/LocalDateExample.java
@@ -0,0 +1,12 @@
+package com.journaldev.java8features;
+
+import java.time.LocalDate;
+
+public class LocalDateExample {
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+
+    }
+
+}
