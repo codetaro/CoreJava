28c75bd35fae3a8b176b1d39a65a15528f292fb2
iquesp <dennis.yuan@terrapages.com>
2019-01-10 15:03:57 +1100

    https://www.journaldev.com/2452/serialization-in-java

---
+++ src/com/journaldev/advanced/Test.java
@@ -0,0 +1,29 @@
+package com.journaldev.advanced;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Balloon red = new Balloon("Red");
+        Balloon blue = new Balloon("Blue");
+
+        swap(red, blue);
+        System.out.println("red color=" + red.getColor());
+        System.out.println("blue color=" + blue.getColor());
+
+        foo(blue);
+        System.out.println("blue color=" + blue.getColor());
+    }
+
+    public static void swap(Object o1, Object o2) {
+        Object temp = o1;
+        o1 = o2;
+        o2 = temp;
+    }
+
+    private static void foo(Balloon balloon) {
+        balloon.setColor("Red");
+        balloon = new Balloon("Green");
+        balloon.setColor("Blue");
+    }
+}
